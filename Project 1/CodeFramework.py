57fd131cb20755cd28dd381aa6648dde0c3165c3
Anney <aromoher@asu.edu>
2022-10-26 11:57:51 -0700

    Code frame work file for reference

---
+++ Project 1/CodeFramework.py
@@ -0,0 +1,154 @@
+# overhead **********************************************************************************************************************************************
+
+import logging
+import math
+import random
+import numpy as np
+import time
+import torch as t
+import torch.nn as nn
+from torch import optim
+from torch.nn import utils
+import matplotlib.pyplot as plt
+
+logger = logging.getLogger(__name__)
+
+# environment parameters **********************************************************************************************************************************************
+
+FRAME_TIME = 0.1  # time interval
+GRAVITY_ACCEL = 0.12  # gravity constant
+BOOST_ACCEL = 0.18  # thrust constant
+
+# system dynamics **********************************************************************************************************************************************
+
+class Dynamics(nn.Module):
+
+    def __init__(self):
+        super(Dynamics, self).__init__()
+
+    @staticmethod
+    def forward(state, action):
+
+        """
+        action: thrust or no thrust
+        state[0] = y
+        state[1] = y_dot
+        """
+        
+        # Apply gravity
+        # Note: Here gravity is used to change velocity which is the second element of the state vector
+        # Normally, we would do x[1] = x[1] + gravity * delta_time
+        # but this is not allowed in PyTorch since it overwrites one variable (x[1]) that is part of the computational graph to be differentiated.
+        # Therefore, I define a tensor dx = [0., gravity * delta_time], and do x = x + dx. This is allowed... 
+        delta_state_gravity = t.tensor([0., GRAVITY_ACCEL * FRAME_TIME])
+
+        # Thrust
+        # Note: Same reason as above. Need a 2-by-1 tensor.
+        delta_state = BOOST_ACCEL * FRAME_TIME * t.tensor([0., -1.]) * action
+
+        # Update velocity
+        state = state + delta_state + delta_state_gravity
+        
+        # Update state
+        # Note: Same as above. Use operators on matrices/tensors as much as possible. Do not use element-wise operators as they are considered inplace.
+        step_mat = t.tensor([[1., FRAME_TIME],
+                            [0., 1.]])
+        state = t.matmul(step_mat, state)
+
+        return state
+    
+# a deterministic controller **********************************************************************************************************************************************
+
+class Controller(nn.Module):
+
+    def __init__(self, dim_input, dim_hidden, dim_output):
+        """
+        dim_input: # of system states
+        dim_output: # of actions
+        dim_hidden: up to you
+        """
+        super(Controller, self).__init__()
+        self.network = nn.Sequential(
+            nn.Linear(dim_input, dim_hidden),
+            nn.Tanh(),
+            nn.Linear(dim_hidden, dim_output),
+            # You can add more layers here
+            nn.Sigmoid()
+        )
+
+    def forward(self, state):
+        action = self.network(state)
+        return action
+    
+# the simulator that rolls out x(1), x(2), ..., x(T) **********************************************************************************************************************************************
+
+class Simulation(nn.Module):
+
+    def __init__(self, controller, dynamics, T):
+        super(Simulation, self).__init__()
+        self.state = self.initialize_state()
+        self.controller = controller
+        self.dynamics = dynamics
+        self.T = T
+        self.action_trajectory = []
+        self.state_trajectory = []
+
+    def forward(self, state):
+        self.action_trajectory = []
+        self.state_trajectory = []
+        for _ in range(T):
+            action = self.controller.forward(state)
+            state = self.dynamics.forward(state, action)
+            self.action_trajectory.append(action)
+            self.state_trajectory.append(state)
+        return self.error(state)
+
+    @staticmethod
+    def initialize_state():
+        state = [1., 0.]  # TODO: need batch of initial states
+        return t.tensor(state, requires_grad=False).float()
+
+    def error(self, state):
+        return state[0]**2 + state[1]**2
+    
+# set up the optimizer **********************************************************************************************************************************************
+
+class Optimize:
+    def __init__(self, simulation):
+        self.simulation = simulation
+        self.parameters = simulation.controller.parameters()
+        self.optimizer = optim.LBFGS(self.parameters, lr=0.01)
+
+    def step(self):
+        def closure():
+            loss = self.simulation(self.simulation.state)
+            self.optimizer.zero_grad()
+            loss.backward()
+            return loss
+        self.optimizer.step(closure)
+        return closure()
+    
+    def train(self, epochs):
+        for epoch in range(epochs):
+            loss = self.step()
+            print('[%d] loss: %.3f' % (epoch + 1, loss))
+            self.visualize()
+
+    def visualize(self):
+        data = np.array([self.simulation.state_trajectory[i].detach().numpy() for i in range(self.simulation.T)])
+        x = data[:, 0]
+        y = data[:, 1]
+        plt.plot(x, y)
+        plt.show()
+        
+# Now it's time to run the code! **********************************************************************************************************************************************
+
+T = 100  # number of time steps
+dim_input = 2  # state space dimensions
+dim_hidden = 6  # latent dimensions
+dim_output = 1  # action space dimensions
+d = Dynamics()  # define dynamics
+c = Controller(dim_input, dim_hidden, dim_output)  # define controller
+s = Simulation(c, d, T)  # define simulation
+o = Optimize(s)  # define optimizer
+o.train(40)  # solve the optimization problem
